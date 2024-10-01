194817e8b8833d3c5b8d632acf349227a170e1aa
Diegoagp <Diegoalegandroquerreroperez@gmail.com>
2024-10-01 13:19:36 -0400

    nueva rama

---
+++ index.js
@@ -0,0 +1,17 @@
+const fn = () => {
+    const miString = 'soy un string'
+    let b = miString + ' y tengo hambre'
+    console.log(b)
+    console.log('hola mundo');
+    
+}
+
+fn()
+
+const peperoni = () => {
+    let a = 'tu pepe y yo roni'
+
+    console.log(a)
+}
+
+peperoni()
